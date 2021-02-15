766bc9d803d99a149e4c7dfaa5da61b0dfcaf359
Villum <62429260+m-v-j@users.noreply.github.com>
2021-02-15 14:32:16 +0100

    Add files via upload

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float speed;
+    Vector2 direction; //(x,y)
+
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        direction = Vector2.one.normalized; //(1,1)
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = direction * speed;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction.y = -direction.y;
+        }
+        else if (collision.gameObject.CompareTag("Paddle"))
+        {
+            speed += Random.Range(0.5f, 1.5f);
+            direction.x = -direction.x;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    public float speed;
+    public float minY;
+    public float maxY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Check if we are left or right paddle
+        //Move the paddle
+        if (gameObject.name == "rightPaddle")
+        {
+            MovePaddle(gameObject.name);
+        }
+        else if (gameObject.name == "leftPaddle")
+        {
+            MovePaddle(gameObject.name);
+        }
+    } 
+    // Update
+
+    void MovePaddle(string paddleName)
+    {
+        float moveY = Input.GetAxisRaw(paddleName) * speed * Time.deltaTime;
+
+        transform.position = new Vector2(
+            transform.position.x,
+           Mathf.Clamp(transform.position.y + moveY,minY,maxY) );
+    }
+
+}
+// Class
